4592eb2e1ca86a5ab86cfaa9edf23868728aee50
Alessandro Marega <ale_marega@hotmail.it>
2021-03-22 17:42:15 +0100

    fix sintassi

--- JSnack-nuovi-3/js/script.js
+++ JSnack-nuovi-3/js/script.js
@@ -3,8 +3,8 @@ const myArray = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let a = parseInt(prompt("inserisci estremo inferiore"));
 let b = parseInt(prompt("inserisci estremo superiore"));
 
-const filtArray = myArray.filter((item, index, array) => {
-  return index >= a && index <= b;
+const filtArray = myArray.filter((item, index) => {
+  return 0 <= a && b <= myArray.length && a <= index && index <= b;
 });
 
 console.log(filtArray);
