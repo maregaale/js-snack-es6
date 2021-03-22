bc9a4e518f1f1e6c972e805edd474fb3ca89ad8b
Alessandro Marega <ale_marega@hotmail.it>
2021-03-22 17:53:22 +0100

    modificato JSnack-3

--- JSnack-nuovi-3/js/script.js
+++ JSnack-nuovi-3/js/script.js
@@ -1,10 +1,8 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 const myArray = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-let a = parseInt(prompt("inserisci estremo inferiore"));
-let b = parseInt(prompt("inserisci estremo superiore"));
 
-const filtArray = myArray.filter((item, index) => {
-  return 0 <= a && b <= myArray.length && a <= index && index <= b;
-});
+function functionName(array, a, b) {
+    return array.filter((item, index) => (0 <= a && b <= array.length && a <= index && index <= b));
+}
 
-console.log(filtArray);
+console.log(functionName(myArray, 3, 4));
